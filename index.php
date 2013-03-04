187b2648e89c9d383ff2a7c3f18c33e6943a9b09
Pranav Gandhi <pranavrulz@gmail.com>
2013-03-04 06:16:10 +0000

    Runs git pull on a dir specified in config.yaml and throws the result into the log file

--- index.php
+++ index.php
@@ -8,7 +8,21 @@ if(@($_POST['payload'] != NULL)){
   $payload = json_decode($_POST['payload']);
 
   # Gets the name of the repository
-  $repo = $payload['repository']['name'];
+  $reponame = $payload->repository;
+  $reponame = $reponame->name;
+
+  # Get all the repositories stored in the config file
+  $repos = get_repositories();
+  if($repos[$reponame] != NULL){
+
+    # Get the directory the repo is in 
+    $dir = $repos[$reponame];
+    # Run the shell command
+    $res = `cd $dir && git pull`;
+
+    # Log the git result into the git log.
+    file_put_contents('git.log', "FOUND $reponame: $res\r\n", FILE_APPEND);
+  }
 }
 
 ?>
