4c184ea62c154ad7e97e05d21e4113b8e4eb3f96
Pranav Gandhi <pranavrulz@gmail.com>
2013-03-04 05:36:11 +0000

    Added a function to get all the repos

--- lib.php
+++ lib.php
@@ -1,8 +1,16 @@
 <?php
 
+# Constant configuration file location
+define("CONFIG_FILE", "config.yaml");
+
 # Throws this into the access.log file
 function add_to_log($log){
   file_put_contents('access.log', $log."\r\n", FILE_APPEND);
+}
+
+# Get the local repositories on this instance
+function get_repositories(){
+  return yaml_parse_file(CONFIG_FILE);
 }
 
 ?>
