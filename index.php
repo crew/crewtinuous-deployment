6c10246c3eb7f94a4e77cbd7df44d7abe5d02823
Pranav Gandhi <pranavrulz@gmail.com>
2013-03-04 05:15:11 +0000

    Convert the payload into a php array, then get the name of the updated repo

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ require('lib.php');
 
 if($_POST['payload'] != NULL){
   add_to_log($_POST['payload']);
+
+  # Decode that sucka!, since its just json
+  $payload = json_decode($_POST['payload']);
+
+  # Gets the name of the repository
+  $repo = $payload['repository']['name'];
 }
 
 ?>
