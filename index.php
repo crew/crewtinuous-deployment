ac69fd9753accb15d06cbc6b23954da4af1d1238
Pranav Gandhi <pranavrulz@gmail.com>
2013-03-04 06:15:38 +0000

    Renamed the function add_to_accesslog because there are two log files

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require('lib.php');
 
-if($_POST['payload'] != NULL){
-  add_to_log($_POST['payload']);
+if(@($_POST['payload'] != NULL)){
+  add_to_accesslog($_POST['payload']);
 
   # Decode that sucka!, since its just json
   $payload = json_decode($_POST['payload']);
--- lib.php
+++ lib.php
@@ -4,7 +4,7 @@
 define("CONFIG_FILE", "config.yaml");
 
 # Throws this into the access.log file
-function add_to_log($log){
+function add_to_accesslog($log){
   file_put_contents('access.log', $log."\r\n", FILE_APPEND);
 }
 
