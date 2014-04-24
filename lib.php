9deafe114c05c709eb2d38e938289ccb31460d36
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-04-23 20:09:26 -0400

    PHPDocs and Style for lib.php

--- lib.php
+++ lib.php
@@ -1,16 +1,22 @@
 <?php
 
-# Constant configuration file location
+/**
+ * The path to the configuration file
+ */
 define("CONFIG_FILE", "config.yaml");
 
-# Throws this into the access.log file
+/**
+ * Logs the given information to the access log
+ * @param $log The information to log
+ */
 function add_to_accesslog($log){
   file_put_contents('access.log', $log."\r\n", FILE_APPEND);
 }
 
-# Get the local repositories on this instance
+/**
+ * Parses the Configuration file and gets a list of repositories that this script should manage
+ * @return mixed The List of repositories to manage and the details regarding them.
+ */
 function get_repositories(){
   return yaml_parse_file(CONFIG_FILE);
 }
-
-?>
